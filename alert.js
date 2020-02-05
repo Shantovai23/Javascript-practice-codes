b70117af23ae86b3ff28f3bffc70f89768ef43a2
shantovai23 <hasibulhasan23sept@gmail.com>
2020-02-05 19:34:54 +0600

    js-practice-code

---
+++ alert.js
@@ -0,0 +1,12 @@
+var a=2;
+        var b=2;
+
+        if(a>b){
+            alert("A is less than B");
+        }
+        else if(a==b){
+            alert("Both are Equal");
+        }
+        else{
+            alert("B is greater than A");
+        }
