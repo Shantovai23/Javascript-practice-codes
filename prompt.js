566fde2f6fef005d7dd78a49b67db0bc709b2f59
Hasibul Hasan Shanto <55630679+Shantovai23@users.noreply.github.com>
2020-02-09 19:48:36 +0600

    Js

---
+++ prompt.js
@@ -0,0 +1,12 @@
+var a= confirm("Do you like Our website?");
+
+       if(a){
+           alert("Thank you");
+       }
+       else{
+           alert("Why not?");
+          
+           var b=prompt("write Down your Oponion");
+           
+           
+       }
