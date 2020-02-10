e67e06a80903cf3a5eb1d6d2fd6f912dd03e2830
Hasibul Hasan Shanto <55630679+Shantovai23@users.noreply.github.com>
2020-02-10 23:33:57 +0600

    Js

---
+++ break_continue.js
@@ -0,0 +1,9 @@
+for(var a=1; a<=30; a++){
+
+        if(a==5){
+            document.write(a+" )Hey I am 5<br>");
+            //continue;
+            break;
+    } 
+    document.write(a+" )Hello<br>");  
+        }
