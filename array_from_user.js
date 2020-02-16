fef605d82ae0b9792a8a2fb6cad20d38eb8073f7
Hasibul Hasan Shanto <55630679+Shantovai23@users.noreply.github.com>
2020-02-17 00:23:55 +0600

    Js

---
+++ array_from_user.js
@@ -0,0 +1,9 @@
+var ary=new Array(3);
+
+      for(var g=0; g<=3; g++){
+          ary[g]=prompt("Enter Value");
+      }
+
+      for(var a=0; a<=3; a++){
+          document.write(ary[a]+"<br>");
+      }
