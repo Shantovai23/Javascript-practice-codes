6950d27c2862f0aec7e7d7201233089989344d62
Hasibul Hasan Shanto <55630679+Shantovai23@users.noreply.github.com>
2020-05-26 19:59:38 +0600

    Js

---
+++ 31-Object-Array.js
@@ -0,0 +1,13 @@
+var x = [
+     {name:'Hasibul',age:20},
+     {name:'Hasan',age:21},
+     {name:'Shanto',age:22},
+    
+ ];
+
+ console.log(x);
+
+
+for (var a=0; a<x.length; a++){
+    document.write(x[a].name+"<br>");
+}
