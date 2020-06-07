4c5f07eb899bfb0d818162991fcb6903e33f106d
Hasibul Hasan Shanto <55630679+Shantovai23@users.noreply.github.com>
2020-06-07 23:35:14 +0600

    Js

---
+++ 32.object_keys_access.js
@@ -0,0 +1,11 @@
+ var x={
+     name: "Shanto",
+     age: 21,
+     city: "Dhaka",
+     email: "demo@gmail.com",
+     hobby: "Coding",
+ };
+
+ for(var i in x){
+     document.write(i+" : "+x[i]+"<br>");
+ }
